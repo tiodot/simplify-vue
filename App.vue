4dd5be0372da02f04a37f70ee31b37555088e710
xiongchengbin <xiongchengbin@meituan.com>
2020-06-05 17:33:32 +0800

    demo演示

--- App.vue
+++ App.vue
@@ -11,6 +11,24 @@ export default {
     return {
       message: '我是一个组件'
     }
-  }
+  },
+  beforeCreate() {
+    console.log('[hook]: beforeCreate')
+  },
+  created() {
+    console.log('[hook]: created')
+  },
+  beforeMount() {
+    console.log('[hook]: beforeMount')
+  },
+  mounted() {
+    console.log('[hook]: mounted')
+  },
+  beforeUpdate() {
+    console.log('[hook]: beforeUpdate')
+  },
+  updated() {
+    console.log('[hook]: updated')
+  },
 }
 </script>
