f6b5642118ab69cf0e5b61cd9a3e01afaddd7fb3
xiongchengbin <xiongchengbin@meituan.com>
2020-06-26 21:27:32 +0800

    渲染子组件示例

--- App.vue
+++ App.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="msg" @click="handleClick">
     {{message}}
+    <Child @click="handleCompEvent"/>
   </div>
 </template>
 
 <script>
+import Child from './Child.vue'
 export default {
   name: 'App',
   data() {
@@ -12,9 +14,15 @@ export default {
       message: '我是一个组件'
     }
   },
+  components: {
+    Child
+  },
   methods: {
     handleClick(evt) {
       console.log('click event', evt.target.tagName)
+    },
+    handleCompEvent(msg) {
+      console.log(msg)
     }
   },
 }
---
+++ Child.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="child" @click="handleClick">
+    {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  data() {
+    return {
+      message: '我是一个子组件'
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.$emit('click', '自动触发子组件点击')
+    }, 1000)
+  },
+}
+</script>
+
+<style>
+  .child {
+    background: #dedede;
+  }
+</style>
